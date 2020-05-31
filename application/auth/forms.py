2b43b151bf5e83428dd3c11ac10c68a2d623285b
Juha Isopoussu <isopoju@gmail.com>
2020-05-31 06:38:19 +0300

    add registration functionality

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Käyttäjätunnus")
     password = PasswordField("Salasana")
   
+    class Meta:
+        csrf = False
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Nimi", [validators.Length(min=3)])
+    username = StringField("Käyttäjätunnus", [validators.Length(min=3)])
+    password = PasswordField("Salasana", [validators.Length(min=3)])
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user
 
 from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -11,7 +11,6 @@ def auth_login():
         return render_template("auth/loginform.html", form = LoginForm())
 
     form = LoginForm(request.form)
-    # mahdolliset validoinnit
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
@@ -23,4 +22,22 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/auth/register", methods = ["GET"])
+def register_form():
+    return render_template("auth/registerform.html", form = RegisterForm())
+
+@app.route("/auth/register", methods = ["POST"])
+def register():
+    form = RegisterForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/registerform.html", form = form)
+
+    new_user = User(form.name.data, form.username.data, form.password.data)
+
+    db.session().add(new_user)
+    db.session().commit()
+  
     return redirect(url_for("index"))
