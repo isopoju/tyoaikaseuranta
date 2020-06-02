4aa9c7871869d054c87b54673bd99bc6f0371a64
Juha Isopoussu <isopoju@gmail.com>
2020-06-02 07:06:20 +0300

    fix project update functionality

--- application/projects/views.py
+++ application/projects/views.py
@@ -7,19 +7,19 @@ from application.projects.forms import ProjectForm
 
 @app.route("/projects", methods=["GET"])
 def projects_index():
-    return render_template("projects/list.html", projects = Project.query.all())
+    return render_template('projects/list.html', projects = Project.query.all())
 
 @app.route("/projects/new/")
 @login_required
 def projects_form():
-    return render_template("projects/new.html", form = ProjectForm())
+    return render_template('projects/new.html', form = ProjectForm())
 
 @app.route("/projects/modify/<project_id>", methods=["GET"])
 @login_required
 def projects_modify(project_id):
     project = Project.query.get(project_id)
 
-    return render_template("projects/update.html", form = ProjectForm(), project = project)
+    return render_template('projects/update.html', form = ProjectForm(), project = project)
 
 @app.route("/projects/update/<project_id>", methods=["POST"])
 @login_required
@@ -29,14 +29,7 @@ def projects_update(project_id):
     project = Project.query.get(project_id)
 
     if not project:
-        return redirect(url_for("project_index")) 
-
-    if not form.validate():
-        return render_template("projects/update.html", form = form)
-
-    if not form.validate_on_submit():
-        error = "Tarkista alkamis- ja päättymispäivämäärät."
-        return render_template("projects/update.html", form = form, error = error)
+        return redirect(url_for('project_index')) 
 
     project.name = form.name.data
     project.description = form.description.data
@@ -44,9 +37,16 @@ def projects_update(project_id):
     project.end_date = form.end_date.data
     project.run = form.run.data
 
+    if not form.validate():
+        return render_template('projects/update.html', form = form, project = project)
+
+    if not form.validate_on_submit():
+        error = "Tarkista alkamis- ja päättymispäivämäärät."
+        return render_template('projects/update.html', form = form, project = project, error = error)
+
     db.session().commit()
   
-    return redirect(url_for("projects_index"))
+    return redirect(url_for('projects_view', project_id=project.id))
 
 @app.route("/projects/", methods=["POST"])
 @login_required
@@ -54,11 +54,11 @@ def projects_create():
     form = ProjectForm(request.form)
 
     if not form.validate():
-        return render_template("projects/new.html", form = form)
+        return render_template('projects/new.html', form = form)
 
     if not form.validate_on_submit():
         error = "Tarkista alkamis- ja päättymispäivämäärät."
-        return render_template("projects/new.html", form = form, error = error)
+        return render_template('projects/new.html', form = form, error = error)
 
     new_project = Project(form.name.data)
     new_project.description = form.description.data
@@ -70,9 +70,9 @@ def projects_create():
     db.session().add(new_project)
     db.session().commit()
   
-    return redirect(url_for("projects_index"))
+    return redirect(url_for('projects_index'))
 
 @app.route("/projects/<project_id>/", methods=["GET"])
 @login_required
 def projects_view(project_id):
-    return render_template("projects/view.html", project = Project.query.get(project_id))
+    return render_template('projects/view.html', project = Project.query.get(project_id))
