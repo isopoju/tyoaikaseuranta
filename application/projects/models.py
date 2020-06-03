af4a892684f4d6d58b9a5d6d7e35c7b63a307421
Juha Isopoussu <isopoju@gmail.com>
2020-06-03 03:17:17 +0300

    add abstract class base and many-to-many table

--- application/projects/models.py
+++ application/projects/models.py
@@ -1,19 +1,24 @@
 from application import db
+from application.models import Base
 
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+attends = db.Table('participation',
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id', ondelete="CASCADE")),
+    db.Column('account_id', db.Integer, db.ForeignKey('account.id', ondelete="CASCADE"))
+)
 
+class Project(Base):
     name = db.Column(db.String(144), nullable=False)
     description = db.Column(db.String(144), nullable=False)
     start_date = db.Column(db.Date, nullable=False)
     end_date = db.Column(db.Date, nullable=False)
-    run = db.Column(db.Boolean, nullable=False)
+    running = db.Column(db.Boolean, nullable=False)
 
     owner_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    participants = db.relationship("User", secondary=attends, backref=db.backref("attending", lazy="dynamic"))
 
-    def __init__(self, name):
+    def __init__(self, name, description, start_date, end_date, running):
         self.name = name
-        self.run = False
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+        self.running = running
