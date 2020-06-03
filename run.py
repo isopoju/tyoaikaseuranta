07d1b32a85bfe1253289a8cc14157839faf6eb4f
Juha Isopoussu <isopoju@gmail.com>
2020-06-03 07:49:50 +0300

    update README.md and run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from application import app
 
+# Heroku logging on:
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
+
 if __name__ == '__main__':
     app.run(debug=True)
